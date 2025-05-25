f6e68659369532e485ae56c22f6e3d63d3282a4e
Thachdong <thachdong270293@gmail.com>
2025-05-25 16:27:18 +0700

    chore: update dependencies and fix tsconfig paths - Updated @nestjs/testing, @nx/jest, and nx to their latest versions. - Downgraded @nx/react to a compatible version. - Updated @nestjs/core, @nestjs/platform-express, @nestjs/platform-socket.io, @nestjs/swagger, and @nestjs/websockets to their latest versions. - Downgraded @nestjs-modules/mailer to a compatible version. - Added missing path mapping for game_caro_ui in tsconfig.base.json.

--- apps/game_caro/module-federation.config.ts
+++ apps/game_caro/module-federation.config.ts
@@ -4,16 +4,7 @@ const config: ModuleFederationConfig = {
   name: 'game_caro',
   exposes: {
     './Module': './src/remote-entry.ts',
-  },
-  shared: (libraryName, defaultConfig) => {
-    if (libraryName === 'react' || libraryName === 'react-dom') {
-      return { ...defaultConfig, singleton: true, requiredVersion: 'auto' };
-    }
-    return defaultConfig;
   }
 };
 
-/**
- * Nx requires a default export of the config to allow correct resolution of the module federation graph.
- **/
 export default config;
--- apps/game_caro/src/app/app.tsx
+++ apps/game_caro/src/app/app.tsx
@@ -1,7 +1,7 @@
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routes/router";
 
-export function App() {
+function App() {
   return <RouterProvider router={router} />
 }
 
