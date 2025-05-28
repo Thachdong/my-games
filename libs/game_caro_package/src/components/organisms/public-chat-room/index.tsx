0bee6d4d549bb0d92eb3837b3152da6b1ce05dfe
Thachdong <thachdong270293@gmail.com>
2025-05-29 00:21:46 +0700

    feat(home): implement HomePage component with layout and integrate PublicChatRoom, GameList, and GameCreation

--- libs/game_caro_package/src/components/organisms/public-chat-room/index.tsx
+++ libs/game_caro_package/src/components/organisms/public-chat-room/index.tsx
@@ -1,5 +1,19 @@
+import { Button, Input } from 'game_caro_package/components/atoms';
 import React from 'react';
 
 export const PublicChatRoom: React.FC = () => {
-  return <div>Public chat room</div>;
+  return (
+    <div className="w-full border rounded-lg bg-white shadow-md max-w-md mx-auto h-full flex flex-col">
+      <p className="font-semibold mb-2 px-4 py-2 bg-blue-100 rounded">
+        Public chat room
+      </p>
+      <div className="mb-4 flex-1 overflow-y-auto bg-gray-50 rounded p-2">
+        Messages
+      </div>
+      <form className='px-2'>
+        <Input name="message" />
+        <Button type="submit">Send</Button>
+      </form>
+    </div>
+  );
 };
---
+++ libs/game_caro_package/src/components/pages/home/index.tsx
@@ -0,0 +1,21 @@
+import { GameCreation } from 'game_caro_package/components/organisms/game-creation';
+import { GameList } from 'game_caro_package/components/organisms/game-list';
+import { PublicChatRoom } from 'game_caro_package/components/organisms/public-chat-room';
+import React from 'react';
+
+export const HomePage: React.FC = () => {
+  return (
+    <div
+      className="grid grid-cols-[275px,1fr,275px] gap-4 mt-8"
+      style={{
+        height: 'calc(100vh - 64px - 32px)',
+      }}
+    >
+      <PublicChatRoom />
+
+      <GameList />
+
+      <GameCreation />
+    </div>
+  );
+};
