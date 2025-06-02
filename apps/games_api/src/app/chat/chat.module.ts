ca61483c978fc5924a3c27ec258a81d73a95ec3f
Thachdong <thachdong270293@gmail.com>
2025-06-02 23:01:30 +0700

    feat: update auth and tournament modules to include Room entity and export TournamentService

--- apps/games_api/src/app/chat/chat.module.ts
+++ apps/games_api/src/app/chat/chat.module.ts
@@ -7,12 +7,16 @@ import { Room, Message } from 'app/chat/entities';
 import { GameModule } from 'app/game/game.module';
 import { UserModule } from 'app/user/user.module';
 import { User } from 'app/user/entities/user.entity';
+import { AuthModule } from 'app/auth/auth.module';
+import { TournamentModule } from 'app/tournament/tournament.module';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([Room, Message, User]),
     GameModule,
     UserModule,
+    AuthModule,
+    TournamentModule,
   ],
   controllers: [ChatController],
   providers: [ChatService, ChatGateway],
